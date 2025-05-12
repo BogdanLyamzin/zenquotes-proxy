bac8925d4f803d9fb816ba6caf6b4a7b2bc01b35
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-05-12 15:52:46 +0200

    fix quotes

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import express from "express";
 import cors from "cors";
-import axios from "axios";
+import quotesy from "quotesy";
 
 const app = express();
 
 app.use(cors());
 
-app.get("/api/random", async(req, res)=> {
+app.get("/api/random", async(_, res)=> {
     try {
-        const {data} = await axios.get("https://zenquotes.io/api/random");
-        res.json(data[0]);
+        const {text: q, author: a} = quotesy.random();
+        res.json({
+            q,
+            a,
+            h: `<blockquote>&ldquo;${q}&rdquo; &mdash; <footer>${a}</footer></blockquote>`
+        });
     }
     catch(error) {
         res.status(400).json({
